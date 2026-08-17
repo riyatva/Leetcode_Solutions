3d3150acddfc1dcec3d7b4492fa1a599900ef9d4
riyatva <101455680+riyatva@users.noreply.github.com>
2026-08-18 05:29:43 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (55.68%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        
-        int n = nums.size();
-        vector<int>prefix(n,0);
-        vector<int>suffix(n,0);
+         int n = nums.size();
+         int sum = 0;
+         for(auto it : nums){
+            sum+=it;
+         }
+         //Calculating prefix and suffix sum in the same loop :
+         // suffix[i] = sum + arr[i] - prefix[i]
+         int left = 0;
+         int right = 0;
+         int ans = -1; 
+         for(int i = 0;i<n;i++){
+            left+=nums[i];
+            right = sum + nums[i] - left;
 
-        int ps = 0;
-        for (int i=0;i<n;i++){
-            prefix[i] = ps + nums[i];
-            ps = prefix[i];
-        }
-        for(auto i : prefix){
-            cout<<i<<" ";
-        }
-     
-        int ss = 0;
-        for(int j=n-1;j>=0;j--){
-            suffix[j] = ss + nums[j];
-            ss = suffix[j];
-        }
-        for(int j=0;j<n;j++){
-            cout<<suffix[j]<<" ";
-        }
-       
-       int ans = -1;
-       for(int i=0;i<n;i++){
-          if(prefix[i] == suffix[i]){
-            ans = i;
-            break;
-          }
-       }
-        return ans;
+            // To find Pivot index :
+            if(left==right){
+                ans = i;
+                break;
+            }
+         }
+       return ans ;    
     }
 };
