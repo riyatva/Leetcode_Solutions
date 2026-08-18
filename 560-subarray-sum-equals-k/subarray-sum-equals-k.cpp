aec60f76204a6225c92f4c31149e0cb4fece4e02
riyatva <101455680+riyatva@users.noreply.github.com>
2026-08-18 07:18:04 +0530

    Time: 67 ms (30.56%) | Memory: 47.8 MB (24.33%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         
-        int n = nums.size();
-        int cnt = 0;
-        for(int i=0;i<n;i++){
-            int sum = 0;
-            for(int j = i;j<n;j++){
-                sum+=nums[j];
-                if(sum == k){
-                    cnt++;
-                }
+            int n = nums.size();
+            map<int,int>mp;
+            int left = 0;
+            int cnt = 0;
+            for(int i=0;i<n;i++){
+              left+=nums[i];
+              // Matlab ab tak ka sum = k toa cnt++ ho jaaygaa.
+              // Nahi toa check karo left - k = prefix pehle kabhi aa chuka hai kya ?
+              if(left == k ){
+                cnt++;
+              }
+              if(mp.find(left-k)!=mp.end()){
+                cnt+=mp[left-k];
+              }
+               mp[left]++;
             }
-        }
-       return cnt ;
+        return cnt;    
     }
 };
 
