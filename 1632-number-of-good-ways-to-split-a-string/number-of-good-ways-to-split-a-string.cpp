84d5c4f6c3856c59f642b5585241e1f7b6ed322e
riyatva <101455680+riyatva@users.noreply.github.com>
2026-09-05 16:43:03 +0530

    Time: 15 ms (34.94%) | Memory: 11.5 MB (70.19%) - LeetSync

---
+++ 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSplits(string s) {
+        int n = s.size();
+        map<char,int>suffix;
+        for(int i=0;i<n;i++){
+            suffix[s[i]]++;
+        }
+        map<char,int>prefix;
+        int cnt = 0;
+        for(int j=0;j<n-1;j++){
+           prefix[s[j]]++;
+           suffix[s[j]]--;
+           if(suffix[s[j]] == 0){
+             suffix.erase(s[j]);
+           }
+           if(prefix.size() == suffix.size()){
+             cnt++;
+           }
+        }
+        return cnt; 
+    }
+};
