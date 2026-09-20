0b6137498abaf5208c2ffc7891c913143dc7c9a3
riyatva <101455680+riyatva@users.noreply.github.com>
2026-09-20 21:30:27 +0530

    Time: 28 ms (20.95%) | Memory: 70.4 MB (80.49%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,57 +1,52 @@
 class Solution {
 public:
-   bool isValid(vector<int>& bloomDay , int mid , int k , int m ){
-             
-      int n = bloomDay.size();
-          
-        //cnt means kitne flowers aa sakte hain .
-        int cnt = 0;
-        int bouq = 0;
-        
-        for(int j=0;j<n;j++){
-            // means flower bloom kar gaya . usko count mein add kar lo.
-            if(bloomDay[j] <= mid){
-                cnt++;
-                if(cnt == k){
-                    bouq++;
-                    cnt = 0;
-                }
-            }
-            else{
-                // cnt ko break karna padegaa kyunki flower bloom nhi kara hai .
+
+     bool isValid(vector<int>&bloomDay ,int m , int k,int mid){
+
+     int n = bloomDay.size();
+     int bouq = 0;
+     int cnt = 0;
+     for(int i=0;i<n;i++){
+
+        if(bloomDay[i] <= mid){
+            // Bloomed :
+            cnt++;
+            if(cnt==k){
+                bouq++;
                 cnt = 0;
-            } 
+            }
         }
+        else{
+            cnt = 0;
+        }
+     }
+     if(bouq >= m){
+        return true;
+     }
 
-       if(bouq >= m){
-           return true;
-       }
-       cout<< mid << " " <<bouq <<endl;
-       
-       return false;
-    }
+       return false ;
+     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-         
-         int n = bloomDay.size();
-         vector<int>copy = bloomDay ;
+        
+        int n = bloomDay.size();
+        int low = 1;
+        int high = 1e9;
+        
+        int ans = -1;
 
-         sort( copy.begin() , copy.end() );
-
-         int low = 1 ;
-         int high = copy[n-1];
-         int ans = -1;
-
-         while(low<=high){
+        while(low<=high){
             int mid = low+(high-low)/2;
-            if(isValid(bloomDay,mid,k,m) == true){
+            if(isValid(bloomDay,m,k,mid)){
                 ans = mid ;
-                high = mid-1;
+                high = mid-1 ;
             }
             else{
                 low = mid+1;
-            } 
-         }
+            }
+        }
 
-        return ans; 
+return ans;
+
+
     }
-}; 
+};
