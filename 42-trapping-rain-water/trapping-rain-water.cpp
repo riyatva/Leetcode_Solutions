21869ee541de44c6903bdf35c097c528a0a70923
riyatva <101455680+riyatva@users.noreply.github.com>
2026-09-24 09:55:25 +0530

    Time: 643 ms (4.61%) | Memory: 27.2 MB (25.51%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -3,43 +3,22 @@ public:
     int trap(vector<int>& height) {
         int n = height.size();
         vector<int>ngr(n,0);
-        stack<int>str; // element hi store karna hai hume.
-        str.push(-1);
         int max_r  = -1e9;
 
         for(int i=n-1;i>=0;i--){
-
-            while(str.top()!=-1 &&  str.top()<height[i]){
-                str.pop();
-            }
-            max_r = max(max_r,str.top());
-            // Kyunki hame  maximum element left or right se matlab hai naki ngr se .
-            ngr[i] = max_r;
-            str.push(height[i]);
+            max_r = max(max_r,height[i]);
+            ngr[i] = max_r  ;
+          
         }
 
-        for(int i=0;i<n;i++){
-            cout<<ngr[i]<<" ";
-        }
+      
 
         vector<int>ngl(n,0);
-        stack<int>stl;
-        stl.push(-1);
         int max_l = -1e9;
-
         for(int i=0;i<n;i++){
-            while(stl.top()!=-1 && stl.top() < height[i]){
-                stl.pop();
-            }
-            max_l = max(max_l,stl.top());
+            max_l = max(max_l,height[i]);
             ngl[i] = max_l;
-            stl.push(height[i]);
-        }
-
-
-        cout<<"ngl"<<endl;    
-        for(int i=0;i<n;i++){
-            cout<<ngl[i]<<" ";
+           
         }
         
         // Ab har element ko check kareingein iski roof pe paani bhar sakta hai ya nahi ?
